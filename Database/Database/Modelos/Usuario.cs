1d279f3cc950e51950dd86e49e56dc770c396b62
Andrew <Andrew@hotmail.com>
2017-05-25 21:30:16 +0100

    Creacion de la clase usuario y del foreach

---
+++ Database/Database/Modelos/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public class Usuario
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
--- Database/Database/db/Db.cs
+++ Database/Database/db/Db.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Database;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -10,9 +11,11 @@ namespace Database
 {
     public class Db
     {
-        public void Conectar()
+        SqlConnection conexion = null;
+
+        public Db()
         {
-            SqlConnection conexion = new SqlConnection();
+            conexion = new SqlConnection();
             try
             {
                 string cadenaConexion = @"Server=.\SQLEXPRESS;
@@ -22,35 +25,104 @@ namespace Database
 
                 conexion.ConnectionString = cadenaConexion;
                 conexion.Open();
-
-                //pregunto por el estado de la conexión
-                if (conexion.State == ConnectionState.Open)
-                {
-                    Console.WriteLine("Conexión abierta con éxito");
-                    conexion.Close();
-                }
-                else
-                {
-                    Console.WriteLine("No se pudo abrir");
-                }
+                Console.WriteLine("Estado de la conexión: " + conexion.State.ToString());
+                EstaLaConexionAbierta();
             }
             catch (SqlException ex)
             {
                 Console.WriteLine("Sql exception " + ex.ToString());
+                Desconectar();
             }
             catch (Exception ex2)
             {
                 Console.WriteLine("Exception " + ex2.ToString());
+                Desconectar();
             }
-            finally
+        }
+
+        //public void Conectar()
+        //{
+        //    conexion = new SqlConnection();
+        //    try
+        //    {
+        //        string cadenaConexion = @"Server=.\SQLEXPRESS;
+        //                                Database=testdb;
+        //                                User Id=testuser;
+        //                                Password = !Curso@2017; ";
+
+        //        conexion.ConnectionString = cadenaConexion;
+        //        conexion.Open();
+        //        Console.WriteLine("Estado de la conexión: " + conexion.State.ToString());
+        //        EstaLaConexionAbierta();
+        //    }
+        //    catch (SqlException ex)
+        //    {
+        //        Console.WriteLine("Sql exception " + ex.ToString());
+        //        Desconectar();
+        //    }
+        //    catch (Exception ex2)
+        //    {
+        //        Console.WriteLine("Exception " + ex2.ToString());
+        //        Desconectar();
+        //    }
+        //}
+
+        public bool EstaLaConexionAbierta()
+        {
+            if(conexion.State == ConnectionState.Open)
             {
-                Console.WriteLine("Nombre de la base de datos " + conexion.Database.ToString());
-                if (conexion != null)
-                {
-                    conexion.Dispose();
-                    conexion = null;
-                }
+                return true;
+            }else
+            {
+                return false;
             }
+        }
+
+        public void Desconectar()
+        {
+            Console.WriteLine("Nombre de la base de datos " + conexion.Database.ToString());
+            if (conexion != null)
+            {
+                if (conexion.State != ConnectionState.Closed)
+                {
+                    conexion.Close();
+                    Console.WriteLine("Conexión cerrada con éxito");
+                }
+                conexion.Dispose();
+                conexion = null;
+            }
+        }
+
+        public List<Usuario> DamelosUsuarios()
+        {
+            // string[] usuarios = new string[45];
+            List<Usuario> usuarios = null;
+            string consultaSQL = "Select * from Users";
+            SqlCommand comando = new SqlCommand(consultaSQL, conexion);
+            SqlDataReader reader = comando.ExecuteReader();
+
+            //usuarios = new Usuario[reader.RecordsAffected];
+            usuarios = new List<Usuario>();
+
+           // int numeroDeUsuarios = 0;
+            while (reader.Read())
+            {
+                usuarios.Add(new Usuario()
+                {
+                    firstName = reader["firstName"].ToString(),
+                    lastName = reader["lastName"].ToString()
+                });
+                //esto es igual
+                //usuarios[numeroDeUsuarios] = new Usuario()
+                //{
+                //    firstName = reader["firstName"].ToString(),
+                //    lastName = reader["lastName"].ToString()
+                //};
+                //usuarios.add(usuarios[numeroDeUsuarios]);
+                //numeroDeUsuarios++;
+                // Console.WriteLine("Nombre: " + reader["firstName"]);
+            }
+            return usuarios;
         }
     }
 }

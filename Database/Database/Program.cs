eadc0150a846597c3135708b0e1fd953504898d2
Andrew <Andrew@hotmail.com>
2017-05-25 19:14:48 +0100

    Creación de conexión a la base de datos

---
+++ Database/Database/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Database
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conectado a al base de datos");
+
+            Db conectar = new Db();
+            conectar.Conectar();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Database/Database/db/Db.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public class Db
+    {
+        public void Conectar()
+        {
+            SqlConnection conexion = new SqlConnection();
+            try
+            {
+                string cadenaConexion = @"Server=.\SQLEXPRESS;
+                                        Database=testdb;
+                                        User Id=testuser;
+                                        Password = !Curso@2017; ";
+
+                conexion.ConnectionString = cadenaConexion;
+                conexion.Open();
+
+                //pregunto por el estado de la conexión
+                if (conexion.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("Conexión abierta con éxito");
+                    conexion.Close();
+                }
+                else
+                {
+                    Console.WriteLine("No se pudo abrir");
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Sql exception " + ex.ToString());
+            }
+            catch (Exception ex2)
+            {
+                Console.WriteLine("Exception " + ex2.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Nombre de la base de datos " + conexion.Database.ToString());
+                if (conexion != null)
+                {
+                    conexion.Dispose();
+                    conexion = null;
+                }
+            }
+        }
+    }
+}

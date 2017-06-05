554fecd52dd6fb8b4671943cf297b850f8699563
Andrew <andrewstyle22@yahoo.es>
2017-06-05 21:10:40 +0100

    ddl.sql y creación de del proyecto ApiCarRental

---
+++ Database/ApiCarRental/Models/MarcasNCoches.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCarRental
+{
+    public class MarcasNCoches {
+        public string marca { get; set; }
+        public int nCoches { get; set; }
+    }
+}
---
+++ Database/ApiCarRental/Models/TipoCombustible.cs
@@ -0,0 +1,7 @@
+﻿namespace ApiCarRental
+{
+    public class TipoCombustible {
+        public long id { get; set; }
+        public string denominacion { get; set; }
+    }
+}
